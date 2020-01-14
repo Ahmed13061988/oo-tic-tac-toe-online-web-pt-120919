2118090619822cdef96d85ddf183d37035fcfabc
Ahmed Hussein <wolktheline@yahoo.com>
2020-01-14 00:13:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,6 +3,18 @@ class TicTacToe
   @board =[" ", " ", " ", " ", " ", " ", " ", " ", " "]
  end 
  
+ WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8]
+   
+   
+   
+   
+   
+   
+   ]
+ 
  
  
  
